91c0e2e43e9dce278c49f369b18bd6ed64e074bb
Matheus <164955715+TooMuchLag@users.noreply.github.com>
2025-12-03 10:10:10 -0300

    Delete backend directory

--- backend/tests/test_config_flow.py
+++
@@ -1,5 +0,0 @@
-from .app.config import settings
-
-print(f"✅ SECRET_KEY carregada: {settings.SECRET_KEY[:10]}...")
-print(f"✅ DATABASE_URL: {settings.DATABASE_URL}")
-print(f"✅ Tipo de EXPIRE_MINUTES: {type(settings.ACCESS_TOKEN_EXPIRE_MINUTES)}")
